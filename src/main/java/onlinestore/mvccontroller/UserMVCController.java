45125fdc135f92609d282265979479cecc0de2b3
George Niculae <george.niculae20@yahoo.com>
2021-02-28 13:02:23 +0200

    Update UserMVCController.java

--- src/main/java/onlinestore/mvccontroller/UserMVCController.java
+++ src/main/java/onlinestore/mvccontroller/UserMVCController.java
@@ -1,7 +1,16 @@
 package onlinestore.mvccontroller;
 
+import onlinestore.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class UserMVCController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserMVCController(UserService userService) {
+        this.userService = userService;
+    }
 }
