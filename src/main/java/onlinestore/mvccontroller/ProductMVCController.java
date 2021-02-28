41c6ee96004c2e132611bd64c1abab69a97677f2
George Niculae <george.niculae20@yahoo.com>
2021-02-28 13:05:19 +0200

    Update ProductMVCController.java

--- src/main/java/onlinestore/mvccontroller/ProductMVCController.java
+++ src/main/java/onlinestore/mvccontroller/ProductMVCController.java
@@ -3,6 +3,8 @@ package onlinestore.mvccontroller;
 import onlinestore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductMVCController {
@@ -14,5 +16,9 @@ public class ProductMVCController {
         this.productService = productService;
     }
 
-    
+    @GetMapping(path = "/products")
+    public String showProducts(Model model) {
+        model.addAttribute("products", this.productService.findAllProducts());
+        return "index";
+    }
 }
