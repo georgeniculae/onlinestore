5ef29f41732ff97f86b6232780dca2563bd4791b
George Niculae <george.niculae20@yahoo.com>
2021-03-08 23:43:55 +0200

    Commit

--- src/main/java/onlinestore/mvccontroller/ProductMVCController.java
+++ src/main/java/onlinestore/mvccontroller/ProductMVCController.java
@@ -1,10 +1,17 @@
 package onlinestore.mvccontroller;
 
+import onlinestore.entity.Product;
 import onlinestore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class ProductMVCController {
@@ -22,5 +29,13 @@ public class ProductMVCController {
         return "products";
     }
 
-
+    @PostMapping(path = "/product/{id}")
+    public String createProduct(@PathVariable("id") @Valid Product product, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new-product";
+        } else {
+            productService.createProduct(product);
+            return "redirect:/products";
+        }
+    }
 }
--- src/main/java/onlinestore/mvccontroller/UserMVCController.java
+++ src/main/java/onlinestore/mvccontroller/UserMVCController.java
@@ -1,10 +1,17 @@
 package onlinestore.mvccontroller;
 
+import onlinestore.entity.Product;
+import onlinestore.entity.User;
 import onlinestore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserMVCController {
@@ -20,5 +27,15 @@ public class UserMVCController {
     public String users(Model model) {
         model.addAttribute("users", this.userService.findAllUsers());
         return "users";
+    }
+
+    @PostMapping(path = "/user/{id}")
+    public String createProduct(@PathVariable("id") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new-user";
+        } else {
+            userService.saveUser(user);
+            return "redirect:/users";
+        }
     }
 }
