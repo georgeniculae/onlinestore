644f9566f52539f3028f8d094257e38a3a92b37d
George Niculae <george.niculae20@yahoo.com>
2021-03-08 22:48:32 +0200

    Commit

--- src/main/java/onlinestore/mvccontroller/UserMVCController.java
+++ src/main/java/onlinestore/mvccontroller/UserMVCController.java
@@ -3,6 +3,8 @@ package onlinestore.mvccontroller;
 import onlinestore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserMVCController {
@@ -12,5 +14,11 @@ public class UserMVCController {
     @Autowired
     public UserMVCController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping(path = "/users")
+    public String users(Model model) {
+        model.addAttribute("users", this.userService.findAllUsers());
+        return "users";
     }
 }
